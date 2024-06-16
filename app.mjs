dcaa62e6f9441d6fd986c4b7ae453ac81353101d
HraZak <hrazak.hz@gmail.com>
2024-06-16 18:27:49 +0200

    fix

--- app.mjs
+++ app.mjs
@@ -5,56 +5,40 @@ import fs from 'fs';
 
 
 const app = express()
+app.use(express.json())
+app.use(express.raw())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.text())
 const port = 3000
 const povereniTemplate = fs.readFileSync('povereni.docx');
 
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-app.post('/test', function (req, res, next) {
-    console.log(req.body)
-    res.json(req.body)
-})
 app.post('/povereni', (req, res) => {
-    const spisovaZn = req.body.spisovaZn ?? ""
-    const datum = req.body.datum ?? ""
-    const povereniKeKontrole = req.body.povereniKeKontrole ?? ""
-    const ustanoveni = req.body.ustanoveni ?? ""
-    const vedouci = req.body.vedouci ?? ""
-    const clen = req.body.clen ?? ""
-    const kontrolovanaOsoba = req.body.kontrolovanaOsoba ?? ""
-    const predmetKontroly = req.body.predmetKontroly ?? ""
-    const kontrolovaneObdobi = req.body.kontrolovaneObdobi ?? ""
-
     createReport({
-        "template": povereniTemplate,
-        "body": req.body,
+        'template': povereniTemplate,
+        'data': {
+            'spisovaZn': req.body.spisovaZn ?? '',
+            'datum': req.body.datum ?? '',
+            'povereniKeKontrole': req.body.povereniKeKontrole ?? '',
+            'ustanoveni': req.body.ustanoveni ?? '',
+            'vedouci': req.body.vedouci ?? '',
+            'clen': req.body.clen ?? '',
+            'kontrolovanaOsoba': req.body.kontrolovanaOsoba ?? '',
+            'predmetKontroly': req.body.predmetKontroly ?? '',
+            'kontrolovaneObdobi': req.body.kontrolovaneObdobi ?? ''
+        }
     }).then(e => {
         res.attachment('test.docx')
             .end(e)
     })
 })
-app.get('/povereni', (req, res) => {
-    createReport({
-        template,
-        "body": {
-            "spisovaZn": 'KUKHK-52/654/2023',
-            "datum": "23.9.2023",
-            "povereniKeKontrole": "č.564/2023",
-            "ustanoveni": "§ 6 odst. 1 zákona č. 298/2014 Sb.",
-            "vedouci": "Lubomír Voral, ev. č. 4659 - vedoucí kontrolní skupiny",
-            "clen": "Zdeněk Týn, ev. č. 899 - člen kontrolní skupiny",
-            "kontrolovanaOsoba": "Miloš Frontman, U Starosty 456, Domanín",
-            "predmetKontroly": "kotel podle zákona § 1 odst. 5 zákona č. 892/2013 Sb.",
-            "kontrolovaneObdobi": "1.1.2012 až 30.12.2015"
-        },
-    }).then(e => {
-        res.attachment('test.docx')
-            .end(e)
-    })
+
+
+
+app.all('/test', function (req, res, next) {
+    console.log(req)
+    console.log(res)
 })
 
 
